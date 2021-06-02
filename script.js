99b7591d4eb466a6558641aecf73b08269cc1ec9
a1kudryavtsev <67130638+a1kudryavtsev@users.noreply.github.com>
2021-06-02 20:06:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ let tattoo = {
 };
 
 function editTattoo() {
-  if (event.target.tagName == "input") { // Если произошло изменение в поле
+  if (event.target.tagName === "INPUT") { // Если произошло изменение в поле
 		tattoo[event.target.name] = event.target.value;
 		calcTattoo(this);
   }
@@ -70,7 +70,7 @@ function calcTattoo(form){
 	
 	// Дополнительные раходники
 	work_material = (tattoo['material-spine'] > 0) ? work_material + Number(tattoo['material-spine']) * 35 : work_material; // доп. иглы
-	work_material = (tattoo['material-dye'] > 0) ? work_material + Number(tattoo['material-spine']) * 20 : work_material; // доп. краска
+	work_material = (tattoo['material-dye'] > 0) ? work_material + Number(tattoo['material-dye']) * 20 : work_material; // доп. краска
 
 	// Скидка за объём работы
 	cost_time = (work_time <= 30) ? cost_time : // Тариф S
@@ -87,9 +87,9 @@ function calcTattoo(form){
 
 	// Акции
 	let promo = (tattoo.promo == "mini-tattoo-500") ? sum = 500 : "";
-
-	sum = Math.ceil(sum / 50) * 50; // Округляем с шагом в 50
-	form.querySelector("input[name=sum]").value = sum;
+	
+  sum = Math.ceil(sum / 50) * 50; // Округляем с шагом в 50
+	form.querySelector("span[name=sum]").textContent = sum;
 }
 
 // Counter -NUM+
@@ -104,4 +104,8 @@ function increaseValue(el) {
   let input = document.querySelector("input[name=" + el.getAttribute("name") + "]");
   let result = input.value < 10 ? input.value = ++input.value : "";
 	input.dispatchEvent(new Event('change', { bubbles: true }));
+}
+
+function fileUpload(el) {
+	el.querySelector("span.calc_file--button").textContent = el.querySelector("input[type=file]").value;
 }
